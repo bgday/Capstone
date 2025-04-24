15ddd222d12ad1ded04633f90af167375bb431a9
Brian Geoffrey Day <brianday03@gmail.com>
2025-04-24 13:53:02 -0600

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// inject year into footer
+const curYear = new Date();
+document.querySelector('#year').textContent = curYear.getFullYear()
+
+document.addEventListener('DOMContentLoaded', function() {
+    const hamMenu = document.querySelector('.hamMenu');
+    const menu = document.querySelector('.menu');
+
+    hamMenu.addEventListener('click', function() {
+      menu.classList.toggle('open');
+    });
+  });
