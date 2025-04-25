70f3a91435255547abc28898c8d7e1bf2f1c680d
Brian Geoffrey Day <brianday03@gmail.com>
2025-04-24 18:49:19 -0600

    style

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,19 @@ const curYear = new Date();
 document.querySelector('#year').textContent = curYear.getFullYear()
 
 document.addEventListener('DOMContentLoaded', function() {
-    const hamMenu = document.querySelector('.hamMenu');
-    const menu = document.querySelector('.menu');
+  const hamMenu = document.querySelector('.hamMenu');
+  const menu = document.querySelector('.menu');
 
-    hamMenu.addEventListener('click', function() {
-      menu.classList.toggle('open');
-    });
+  hamMenu.addEventListener('click', function() {
+
+    menu.classList.toggle('open');
+    
+    hamMenu.classList.toggle('open');
+    
+    if (hamMenu.classList.contains('open')) {
+      hamMenu.innerHTML = '&times;';
+    } else {
+      hamMenu.innerHTML = '&#9776;';
+    }
   });
+});
